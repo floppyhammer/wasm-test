0365747a488bf0e78259d3dccbe2d8f59ed46f4e
floppyhammer <tannhauser_chen@outlook.com>
2022-12-20 17:25:41 +0800

    Change Powershell script to batch

--- emcc-compile.bat
+++ emcc-compile.bat
@@ -1,6 +1,8 @@
 D:
 cd D:/Dev/Projects/wasm-test
+
 D:/Env/wasm/emsdk/emsdk activate latest
+
 D:/Env/wasm/emsdk/upstream/emscripten/emcc -s USE_GLFW=3 -s FULL_ES3=1 main.c -o index.html
 
-$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+pause
